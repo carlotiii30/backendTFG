1406b64202d9e13dd5a36d40bb4a1e1a4c8d08c5
Carlota de la Vega <carlotadlavega@correo.ugr.es>
2024-02-05 18:09:30 +0100

    Configurado servidor en Python. Closes #7

---
+++ src/server.py
@@ -0,0 +1,26 @@
+import socket
+
+# Configurar el servidor
+HOST = 'localhost'
+PORT = 8000
+
+# Crear un socket TCP/IP
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+    # Vincular el socket a la dirección y el puerto especificados
+    server_socket.bind((HOST, PORT))
+    # Escuchar conexiones entrantes
+    server_socket.listen()
+
+    print("Esperando conexiones entrantes...")
+
+    # Aceptar la conexión entrante
+    client_socket, client_address = server_socket.accept()
+
+    with client_socket:
+        print('Conexión establecida desde', client_address)
+        while True:
+            # Recibir datos del cliente
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            print('Mensaje recibido:', data.decode())
