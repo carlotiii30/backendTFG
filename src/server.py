9984c208ff3dcdc07a89b617020dccb1adec08bc
Carlota de la Vega <carlotadlavega@correo.ugr.es>
2024-02-05 18:41:56 +0100

    Manejando errores. Closes #8

--- src/server.py
+++ src/server.py
@@ -6,21 +6,24 @@ PORT = 8000
 
 # Crear un socket TCP/IP
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-    # Vincular el socket a la dirección y el puerto especificados
-    server_socket.bind((HOST, PORT))
-    # Escuchar conexiones entrantes
-    server_socket.listen()
+    try:
+        # Vincular el socket a la dirección y el puerto especificados
+        server_socket.bind((HOST, PORT))
+        # Escuchar conexiones entrantes
+        server_socket.listen()
 
-    print("Esperando conexiones entrantes...")
+        print("Esperando conexiones entrantes en el puerto", PORT)
 
-    # Aceptar la conexión entrante
-    client_socket, client_address = server_socket.accept()
+        # Aceptar la conexión entrante
+        client_socket, client_address = server_socket.accept()
 
-    with client_socket:
-        print('Conexión establecida desde', client_address)
-        while True:
-            # Recibir datos del cliente
-            data = client_socket.recv(1024)
-            if not data:
-                break
-            print('Mensaje recibido:', data.decode())
+        with client_socket:
+            print('Conexión establecida desde', client_address)
+            while True:
+                # Recibir datos del cliente
+                data = client_socket.recv(1024)
+                if not data:
+                    break
+                print('Mensaje recibido:', data.decode())
+    except Exception as e:
+        print("Error en el servidor:", e)
